0ba3b283e3364107fc971e1ea265373eb772a8cb
Saiful-huq <s.md.saifulhuq007@gmail.com>
2023-12-01 07:59:33 -0800

    LC 442

--- sorting/FindAllDuplicatesinanArray.java
+++ sorting/FindAllDuplicatesinanArray.java
@@ -1,33 +1,37 @@
 package DSA.algos.sorting;
 //442. Find All Duplicates in an Array
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class FindAllDuplicatesinanArray {
     public static void main(String[] args) {
         int[] nums = {4,3,2,7,8,2,3,1};
-
-       // System.out.println(dupli(nums));
+        findDuplicates(nums);
+        System.out.println(Arrays.toString(nums));
     }
-    /*static List<Integer> findDuplicates(int[] nums){
-        its pending when you are free solve this!! bro!
-    }
-*/
-
-
-
-
-
-
-    // this will give a how many duplicates
-    /*static int dupli(int[] nums){
-        int ans =0;
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i+1; j < nums.length ; j++) {
-                if (nums[i]==nums[j]){
-                    ans++;
-                }
+    static List<Integer> findDuplicates(int[] nums){
+        int i=0;
+        while(i< nums.length){
+            int correct = nums[i] -1   ;
+            if (nums[i]!=nums[correct]){
+                swap(nums,i,correct);
+            }else{
+                i++;
             }
         }
-        return ans;
-    }*/
+        List<Integer> ans=new ArrayList<>();
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j]!=j+1){
+                ans.add(nums[j]);
+            }
+        }return ans;
+    }
+
+
+    static void swap(int[]nums,int i,int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j]=temp;
+    }
 }
