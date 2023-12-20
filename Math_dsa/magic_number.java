0e127d865e2a3c30116fa62b478dc2c2c31b919d
saiful <s.md.saifulbuq007@gmail.com>
2023-12-21 00:32:48 +0530

    Magic number by amazon

---
+++ Math_dsa/magic_number.java
@@ -0,0 +1,16 @@
+package DSA.algos.Math_dsa;
+
+public class magic_number {
+    public static void main(String[] args) {
+        int n=6;
+        int ans=0;
+        int base=5;
+        while(n>0){
+            int last = n & 1;
+            n = n >> 1;
+            ans= ans + last * base;
+            base= base * 5;
+        }
+        System.out.println(ans);
+    }
+}
